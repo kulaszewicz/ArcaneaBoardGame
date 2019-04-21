e7713d3c067b103396bfa2eeb8f2f87b69b56c90
kulaszewicz <s17838@pjwstk.edu.pl>
2019-04-21 23:14:04 +0200

    Rewrite hexMap generation to more readable version using more of es6 stuff and changed convention to be based mostly on functions. Added attachedValidator to check if next generated hex won't be sticking to any previous one besides the last one, mapReset function to reset the process if generator gets hard stuck (gonna change latter, when i get the math right), made compass dynamic using setCompass function and prepared 3 presets based on difficulty(1-3).
    I do not remember any more sins and I heartily regret them all

--- js/hexMap.js
+++ js/hexMap.js
@@ -2,6 +2,7 @@
 
     const HexCompass = {
         moveHistory: [],
+        stuck: 0,
         //Moves (move index, left, bottom), start has index of 0 and end 7
         S: [1, 0, -4],
         SW: [2, -3, -2],
@@ -22,13 +23,14 @@
     HexMap.prototype.generateMap = function () {
         console.log('Started generating');
         // Start
-        let Hex = new HexInst(0, [0, 0], [0, 0], true);
-        Hex.DrawHex();
-        HexCompass.moveHistory.push(0);
+        let Hex = new HexInst(0, [0, 0], [0, 0], true); // 1st Hex bottom left
+        HexCompass.moveHistory.push([0,0]);
         for (let i = 0; i < 20; i++){
             Hex.NextMove(HexCompass);
-            Hex.DrawHex();
-            //HexCompass.moveHistory.push(move[0]);
+        }
+
+        for (let i = 0; i < HexCompass.stuck; i++){
+            Hex.NextMove(HexCompass);
         }
 
         if (this.difficulty === 'Easy') {
@@ -41,12 +43,16 @@
             console.log(`There was an error with handling the difficulty, you passed: ${this.difficulty}`);
         }
 
+
+        Hex.DrawMap();
+
         console.log('Generating has ended');
+
+        console.log(HexCompass.moveHistory);
     };
 
 
     const HexInst = function (lastDirection, currPoss, lastPoss, haveEvent) {
-
         this.lastDirection = lastDirection;
         this.currPoss = currPoss;
         this.lastPoss = lastPoss;
@@ -60,55 +66,55 @@
         rPath0 = Math.floor((Math.random() * 2) + 1);
         rPath = Math.floor((Math.random() * 3) + 1);
 
-        if(this.lastDirection === 0){
+        if(this.lastDirection === 0){ // Start
             if (rPath0 === 1) {
                 cPath = compass.N;
             } else cPath = compass.NE;
             this.SetMove(cPath);
 
-        } else if (this.lastDirection === 1){
+        } else if (this.lastDirection === 1){ //S
             if(rPath === 1){
                 cPath = compass.SW;
-            } else  if (rPath ===2) {
+            } else  if (rPath === 2) {
                 cPath = compass.S;
             } else cPath = compass.SE;
             this.SetMove(cPath);
 
-        } else if (this.lastDirection === 2){
+        } else if (this.lastDirection === 2){ // SW
             if(rPath === 1){
                 cPath = compass.NW;
-            } else  if (rPath ===2) {
+            } else  if (rPath === 2) {
                 cPath = compass.SW;
             } else cPath = compass.S;
             this.SetMove(cPath);
 
-        }else if (this.lastDirection === 3){
+        }else if (this.lastDirection === 3){ // NW
             if(rPath === 1){
                 cPath = compass.N;
-            } else  if (rPath ===2) {
+            } else  if (rPath === 2) {
                 cPath = compass.NW;
             } else cPath = compass.SW;
             this.SetMove(cPath);
 
-        } else if (this.lastDirection === 4){
+        } else if (this.lastDirection === 4){ // N
             if(rPath === 1) {
                 cPath = compass.NW;
-            } else  if (rPath ===2) {
+            } else  if (rPath === 2) {
                 cPath = compass.N;
             } else cPath = compass.NE;
             this.SetMove(cPath);
-        } else if (this.lastDirection === 5){
+        } else if (this.lastDirection === 5){ // NE
             if(rPath === 1){
                 cPath = compass.N;
-            } else  if (rPath ===2) {
+            } else  if (rPath === 2) {
                 cPath = compass.NE;
             } else cPath = compass.SE;
             this.SetMove(cPath);
 
-        } else if (this.lastDirection === 6){
+        } else if (this.lastDirection === 6){ // SE
             if(rPath === 1){
                 cPath = compass.NE;
-            } else  if (rPath ===2) {
+            } else  if (rPath === 2) {
                 cPath = compass.SE;
             } else cPath = compass.S;
             this.SetMove(cPath);
@@ -120,7 +126,7 @@
     /**
      * @return {boolean}
      */
-    HexInst.prototype.SizeValidator = function(cPath) {
+    HexInst.prototype.SizeValidator = function(cPath)  {
 
         let tempPossLeft =  this.currPoss[0];
         let tempPossBottom =  this.currPoss[1];
@@ -131,29 +137,80 @@
         return !(tempPossLeft < 0 || tempPossLeft > 100 || tempPossBottom < 0 || tempPossBottom > 100);
     };
 
-    HexInst.prototype.SetMove = function(cPath) {
-        if (this.SizeValidator(cPath) === true){
-            this.lastDirection = cPath[0];
-            this.currPoss[0] += cPath[1];
-            this.currPoss[1] += cPath[2];
+    /**
+     * @return {boolean}
+     */
+    HexInst.prototype.LoopValidator = function(history, cP) {
+        for (let i = 1; i < history.length; i++){
+           if (cP[0] === history[i-1][0] && cP[1] === history[i-1][1]){ /// Search hex positions history and look if there is one with same coordinates
+               return false;
+           }
         }
-        else this.NextMove(HexCompass);
+        return true;
     };
 
 
-    HexInst.prototype.DrawHex = function () {
+    HexInst.prototype.isStuckValidator = function(history) {
+
+    };
+
+    HexInst.prototype.SetMove = function(cPath) {
+
+        if (HexCompass.stuck > 20) this.Reset();
+        else {
+            if (this.SizeValidator(cPath) === true && this.LoopValidator(HexCompass.moveHistory, this.currPoss) === true) {
+                this.lastDirection = cPath[0];
+                this.currPoss[0] += cPath[1];
+                this.currPoss[1] += cPath[2];
+
+
+                HexCompass.moveHistory.push([this.currPoss[0], this.currPoss[1], this.lastDirection]);
+
+            } else if (this.SizeValidator(cPath) === false && this.lastDirection === 1 || this.lastDirection === 4) {
+                HexCompass.stuck++;
+
+                HexCompass.moveHistory.pop();
+
+                this.currPoss[0] = HexCompass.moveHistory[HexCompass.moveHistory.length - 1][0];
+                this.currPoss[1] = HexCompass.moveHistory[HexCompass.moveHistory.length - 1][1];
+                this.lastDirection = HexCompass.moveHistory[HexCompass.moveHistory.length - 1][2];
+
+                this.NextMove(HexCompass);
+            } else this.NextMove(HexCompass);
+        }
+    };
+
+    HexInst.prototype.Reset = function (){
+            console.log("reset");
+            HexCompass.moveHistory = 0;
+            this.currPoss[0] = 0;
+            this.currPoss[1] = 0;
+            this.lastDirection = 0;
+            this.NextMove(HexCompass);
+    };
+
+    HexInst.prototype.DrawMap = function () {
+
+        const generatedMap = HexCompass.moveHistory;
         //Drawing single hex
-        let curr = document.createElement('hex');
-        curr.style.setProperty("--currLeft", this.currPoss[0] + "%");
-        curr.style.setProperty("--currBottom", this.currPoss[1] + "%");
-        curr.className = "hex";
-        let frame = document.getElementById("frame");
-        frame.appendChild(curr);
+        for (let i = 0; i < generatedMap.length; i++){
+            let curr = document.createElement('hex');
+            curr.style.setProperty("--currLeft", generatedMap[i][0] + "%");
+            curr.style.setProperty("--currBottom", generatedMap[i][1] + "%");
+            curr.innerHTML = `${i}`;
+            curr.className = "hex";
+            let frame = document.getElementById("frame");
+            frame.appendChild(curr);
+        }
     };
+
 
 
 
     let testMap = new HexMap('Easy', 0, true);
 
     testMap.generateMap();
+
+
 }
+
---
+++ js/hexMap2.js
@@ -0,0 +1,216 @@
+
+class HexMap2 {
+
+    constructor(difficulty, numOfBoss, isActive) {
+        this.difficulty = difficulty;
+        this.numOfBoss = numOfBoss;
+        this.isActive = isActive;
+    };
+
+    HexProps = {
+        moveHistory: [[4,0,0]], // direction, x, y
+        stuck: 0,
+        skips:0,
+    };
+
+
+
+    generateMap = () => {
+        console.log('Started generating');
+
+        const HexCompass = this.setCompass(this.setCompassSize()[0]);
+
+        const size = this.setCompassSize()[1];
+        console.log(size);
+
+        console.log(HexCompass);
+
+        this.hexNextMove(this.HexProps.moveHistory, 1, (size-1), HexCompass);
+
+       /* for (let i = 0; i < this.HexProps.stuck; i++){
+            console.log("yol");
+            this.hexNextMove(this.HexProps.moveHistory, i);
+        }*/
+
+        console.log('Generating has ended');
+
+        console.log(this.HexProps.stuck + " times stuck");
+        console.log(this.HexProps.skips + " times skipped");
+        console.log(this.HexProps.moveHistory );
+    };
+
+    hexNextMove = (moveHistory, index, size, HexCompass) => {
+
+        if (index > size) {
+            console.log("END");
+            this.drawMap(this.HexProps.moveHistory);
+        }
+        else {
+
+            let cDir, rDir; // Chosen direction, Random direction
+            //console.log(index);
+            //console.log(moveHistory);
+            const lastHexDirection = moveHistory[(index - 1)][0];
+
+            rDir = Math.floor((Math.random() * 3) + 1);
+            //console.log(rDir);
+
+            cDir = this.chosenDirection(lastHexDirection, rDir);
+
+
+            if (cDir === 0) {
+                cDir = HexCompass[5].slice(0);
+            } else if (cDir === 7) {
+                cDir = HexCompass[0].slice(0);
+            } else {
+                cDir = HexCompass[(cDir - 1)].slice(0);
+            }
+            this.hexSetMove(cDir, moveHistory, index, size, HexCompass);
+        }
+
+    };
+
+    chosenDirection = (lastDirection, rDirection) => {
+      switch (rDirection) {
+          case 1:
+              return (lastDirection + 1);
+          case 2:
+              return (lastDirection - 1);
+          default:
+              return lastDirection;
+      }
+    };
+
+    hexSetMove = (cDir, moveHistory, index, size, HexCompass) => {
+
+        if (this.HexProps.stuck >= size || this.HexProps.skips >= (size*2)){
+            this.hexMapReset(size, HexCompass);
+        } else {
+
+            //console.log(cDir);
+            const lastDirection = index > 0 ? moveHistory[(index - 1)][0] : moveHistory[index][0];
+            const proposedHex = [cDir[0], cDir[1] + moveHistory[(index - 1)][1], cDir[2] + moveHistory[(index - 1)][2]];
+
+            if (this.sizeValidator(proposedHex) && this.loopValidator(proposedHex, moveHistory) && this.attachedValidator(proposedHex, moveHistory, index)) {
+
+                //console.log("good");
+                //console.log(JSON.parse(JSON.stringify(this.HexCompass)));
+                this.HexProps.moveHistory.push(proposedHex);
+                index++;
+                this.hexNextMove(moveHistory, index, size, HexCompass);
+
+            } else if (this.sizeValidator(proposedHex, moveHistory, index) === false && lastDirection === 1 || lastDirection === 4 && index !== 1) {
+                //console.log("stuck");
+                this.HexProps.stuck++;
+                moveHistory.pop();
+                index--;
+                this.hexNextMove(moveHistory, index, size, HexCompass);
+            }else if (this.attachedValidator(proposedHex, moveHistory, index) === false) {
+                console.log('attached');
+                moveHistory.pop();
+                index--;
+                this.hexNextMove(moveHistory, index, size, HexCompass);
+            }
+            else {
+                //console.log("skip");
+                this.HexProps.skips++;
+                this.hexNextMove(moveHistory, index, size, HexCompass);
+            }
+        }
+    };
+
+
+    drawMap = (moveHistory) => {
+
+        const generatedMap = moveHistory;
+        //Drawing hexes based on generated array
+        for (let i = 0; i < generatedMap.length; i++){
+            let curr = document.createElement('hex');
+            curr.style.setProperty("--currLeft", generatedMap[i][1] + "%");
+            curr.style.setProperty("--currBottom", generatedMap[i][2] + "%");
+            curr.style.setProperty("--setSize", this.setCompassSize()[0] + "%");
+            curr.innerHTML = `${i+1}`;
+            curr.className = "hex";
+            let frame = document.getElementById("frame");
+            frame.appendChild(curr);
+        }
+
+    };
+
+    // Validators
+
+    sizeValidator = (proposedHex) => {
+        const tempPossLeft = proposedHex[1];
+        const tempPossBottom =  proposedHex[2];
+
+        return !(tempPossLeft < 0 || tempPossLeft > 95 || tempPossBottom < 0 || tempPossBottom > 95);
+    };
+
+    loopValidator = (proposedHex, moveHistory) => {
+        const tempPossLeft = proposedHex[1];
+        const tempPossBottom =  proposedHex[2];
+
+        for (let i = 1; i < moveHistory.length; i++){
+            //console.log(tempPossLeft+" X");
+            //console.log(tempPossBottom+" Y");
+            if (tempPossLeft === moveHistory[i-1][1] && tempPossBottom === moveHistory[i-1][2]){/// Search hex positions history and look if there is one with same coordinates
+                return false;
+            }
+        }
+        return true;
+    };
+
+    attachedValidator = (proposedHex, moveHistory, index) => {
+        const tempPossLeft = proposedHex[1];
+        const tempPossBottom =  proposedHex[2];
+
+        for (let i = 1; i < moveHistory.length; i++){
+            if ((tempPossLeft - moveHistory[i-1][1]) < 3 && (index - i) > 3){
+                return false;
+            }else if((tempPossBottom - moveHistory[i-1][1]) < 3 && (index - i) > 3){
+                return false;
+            }
+        }
+        return true;
+    };
+    // Hard reset
+
+    hexMapReset = (size, HexCompass) => {
+        console.log("HARD RESET");
+        this.HexProps.stuck = 0;
+        this.HexProps.skips = 0;
+        this.HexProps.moveHistory = [[4,0,0]];
+        this.hexNextMove(this.HexProps.moveHistory, 1, size, HexCompass);
+    };
+
+    setCompassSize = () => {
+        switch (this.difficulty) {
+            case 1:
+                return  [8, 20];
+            case 2:
+                return  [6, 35];
+            case 3:
+                return  [4, 50];
+            default:
+                return  [8, 20];
+        }
+    };
+
+    setCompass = (size) => {
+        return [
+            //Moves (move index, left, bottom), start has index of 0 and end 7
+            [1, 0, -size], // S
+            [2, (-0.75)*size, (-0.5)*size], // SW
+            [3, (-0.75)*size, (0.5)*size], // NW
+            [4, 0, size], // N
+            [5, (0.75)*size ,(0.5)*size], // NE
+            [6, (0.75)*size, (-0.5)*size] // SE
+        ];
+    }
+
+}
+
+const testMap = new HexMap2(1, 2, true);
+
+testMap.generateMap();
+
